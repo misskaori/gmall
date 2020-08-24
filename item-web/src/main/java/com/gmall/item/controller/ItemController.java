01a2d2403156e5657d35eb9b4de69dea8949aa58
misskaori <liuweizhe@hotmail.com>
2020-08-24 16:17:57 +0800

    2020.08.24 Push Adding redis cache mechanism. And add the redis distributed lock to avoid the problem of excessive MySQL pressure during high concurrency

--- item-web/src/main/java/com/gmall/item/controller/ItemController.java
+++ item-web/src/main/java/com/gmall/item/controller/ItemController.java
@@ -1,8 +1,10 @@
 package com.gmall.item.controller;
 
 import com.alibaba.dubbo.config.annotation.Reference;
+import com.alibaba.fastjson.JSON;
 import com.gmall.bean.PmsProductSaleAttr;
 import com.gmall.bean.PmsSkuInfo;
+import com.gmall.bean.PmsSkuSaleAttrValue;
 import com.gmall.service.SkuService;
 import com.gmall.service.SpuService;
 import org.springframework.stereotype.Controller;
@@ -12,7 +14,9 @@ import org.springframework.web.bind.annotation.RequestMapping;
 
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 @Controller
 public class ItemController {
@@ -41,6 +45,23 @@ public class ItemController {
         //sale attr collection
         List<PmsProductSaleAttr> pmsProductSaleAttrs = spuService.spuSaleAttrListCheckBySku(pmsSkuInfo.getProductId(), pmsSkuInfo.getId());
         map.put("spuSaleAttrListCheckBySku", pmsProductSaleAttrs);
+        // 查询当前sku的spu的其他sku的集合的hash表
+        Map<String, String> skuSaleAttrHash = new HashMap<>();
+        List<PmsSkuInfo> pmsSkuInfos = skuService.getSkuSaleAttrValueListBySpu(pmsSkuInfo.getProductId());
+
+        for (PmsSkuInfo skuInfo : pmsSkuInfos) {
+            String k = "";
+            String v = skuInfo.getId();
+            List<PmsSkuSaleAttrValue> skuSaleAttrValueList = skuInfo.getSkuSaleAttrValueList();
+            for (PmsSkuSaleAttrValue pmsSkuSaleAttrValue : skuSaleAttrValueList) {
+                k += pmsSkuSaleAttrValue.getSaleAttrValueId() + "|";// "239|245"
+            }
+            skuSaleAttrHash.put(k,v);
+        }
+
+        // 将sku的销售属性hash表放到页面
+        String skuSaleAttrHashJsonStr = JSON.toJSONString(skuSaleAttrHash);
+        map.put("skuSaleAttrHashJsonStr",skuSaleAttrHashJsonStr);
         return "item";
     }
 
--- manage-service/src/test/java/com/gmall/manage/ManageServiceApplicationTests.java
+++ manage-service/src/test/java/com/gmall/manage/ManageServiceApplicationTests.java
@@ -1,21 +1,26 @@
 package com.gmall.manage;
 
 import com.gmall.manage.mapper.PmsProductSaleAttrMapper;
+import com.gmall.util.RedisUtil;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
+import redis.clients.jedis.Jedis;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class ManageServiceApplicationTests {
+    @Autowired
+    RedisUtil redisUtil;
 
     @Autowired
     PmsProductSaleAttrMapper pmsProductSaleAttrMapper;
     @Test
     public void contextLoads() {
-        System.out.println(pmsProductSaleAttrMapper.selectSpuSaleAttrListCheckBySku("25", "106"));
+        Jedis jedis =redisUtil.getJedis();
+        System.out.println(jedis);
     }
 
 }
